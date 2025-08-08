c9879cad116fb41351555853d4135dfbda30c8f8
Guilherme Oliveira <oliveiraguilherme.pro@gmail.com>
2025-08-08 11:26:38 -0300

    feat: add storybook

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from 'eslint-plugin-storybook';
+
 import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
@@ -7,21 +10,24 @@ import prettier from 'eslint-plugin-prettier/recommended';
 import pluginQuery from '@tanstack/eslint-plugin-query';
 import { globalIgnores } from 'eslint/config';
 
-export default tseslint.config([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+export default tseslint.config(
+  [
+    globalIgnores(['dist']),
+    {
+      files: ['**/*.{ts,tsx}'],
+      extends: [
+        js.configs.recommended,
+        tseslint.configs.recommended,
+        reactHooks.configs['recommended-latest'],
+        reactRefresh.configs.vite,
+      ],
+      languageOptions: {
+        ecmaVersion: 2020,
+        globals: globals.browser,
+      },
     },
-  },
-  prettier,
-  ...pluginQuery.configs['flat/recommended'],
-]);
+    prettier,
+    ...pluginQuery.configs['flat/recommended'],
+  ],
+  storybook.configs['flat/recommended']
+);
